2e492cdefddc613980a3467ae4d560027977f358
JosepDev23 <josepdev23@gmail.com>
2024-05-29 12:55:32 +0200

    implementation: git --help

--- app/services/console.service.ts
+++ app/services/console.service.ts
@@ -11,20 +11,36 @@ function checkCommand(
     return `Command ${splittedCommand[0]} isn't recognized yet`
   else {
     switch (splittedCommand[1]) {
+      case '--help':
+        return (
+          'editfile <filename>   Make changes to <filename>\n' +
+          'status                Show the working tree status\n' +
+          'add                   Add file contents to the index\n'
+        )
       case 'editfile':
         if (!splittedCommand[2]) return 'Usage: git editfile <filename>'
         context.makeChanges(splittedCommand[2])
         return `File ${splittedCommand[2]} edited successfully!`
       case 'status':
-        return (
-          `On branch master\n` +
-          `Changes not staged for commit:\n` +
-          `(use "git add <file>..." to update what will be committed)\n` +
-          `(use "git restore <file>..." to discard changes in working directory)\n` +
-          context.workingDirectory
-            .map((file) => `    modified:   ${file}`)
-            .join('\n')
-        )
+        if (context.workingDirectory.length > 0)
+          return (
+            `On branch master\n` +
+            `Changes not staged for commit:\n` +
+            `(use "git add <file>..." to update what will be committed)\n` +
+            `(use "git restore <file>..." to discard changes in working directory)\n` +
+            context.workingDirectory
+              .map((file) => `    modified:   ${file}`)
+              .join('\n')
+          )
+        else return 'Nothing to commit, working tree clean'
+      case 'add':
+        if (!splittedCommand[2])
+          return 'Usage: git add <filename> or git add --all'
+        else if (splittedCommand[2] === '--all') {
+          context.workingDirectory.forEach((file) => {
+            context.moveFromWorkingDirectoryToStagingArea(file)
+          })
+        }
       default:
         return `git: '${splittedCommand[1]}' is not a git command. See 'git --help'`
     }
