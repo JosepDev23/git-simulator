b105bde43992d8e28ed4b8c011a14f6076fec43b
JosepDev23 <josepdev23@gmail.com>
2024-07-02 20:55:01 +0200

    cool font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
 import './globals.css'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Git Simulator',
   description: 'Git simulator',
-  icons: {
-    other: [
-      { rel: 'preconnect', url: 'https://fonts.googleapis.com' },
-      { rel: 'preconnect', url: 'https://fonts.gstatic.com' },
-      {
-        rel: 'stylesheet',
-        url: 'https://fonts.googleapis.com/css2?family=Source+Code+Pro:ital,wght@0,200..900;1,200..900&display=swap',
-      },
-    ],
-  },
 }
 
 export default function RootLayout({
@@ -26,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
